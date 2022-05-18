fef55a8077c2a109c95e929a35b8772deb856fbe
bfbarry <barry.brian.f@gmail.com>
2022-05-18 13:01:45 -0700

    re for aliases

--- list_aliases.py
+++ list_aliases.py
@@ -1,9 +1,7 @@
 """
-can't figure proper way to do with re (lookbacks)
+re (lookbacks)
 - see my post: https://stackoverflow.com/questions/72209391/regex-to-match-text-after-starting-word-of-a-line
 - https://stackoverflow.com/questions/40728902/regex-match-after-word
-
-just using a shell script for this now
 """
 import re
 from os.path import basename, normpath
@@ -17,16 +15,6 @@ RC_FILE = RC_LOC[sys.argv[1]]
 with open(RC_FILE, 'r') as f:
     rc = f.read()
 
-# pat = re.compile('^alias') # https://stackoverflow.com/questions/5006716/getting-the-text-that-follows-after-the-regex-match
+# https://stackoverflow.com/questions/5006716/getting-the-text-that-follows-after-the-regex-match
 print(f'Aliases in {basename(normpath(RC_FILE))}:\n')
-# [print(a) for a in re.findall(pat, rc)[1:-1]] # omit first comment and alias for this script
-
-## Using loop instead
-for l in rc.split('\n'):
-    line = l.split()
-    try:
-        if line[0] == 'alias' and 'aliases' not in line[1]:
-            print(l.replace('alias ', ''))
-    except:
-        continue
-    
+[print(a) for a in re.findall(r'^alias (.*)$', rc, re.M) if __file__ not in a] # omit alias for this script
