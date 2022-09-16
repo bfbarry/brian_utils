639ee343c981da5dab893843923719529ddf1f08
bfbarry <barry.brian.f@gmail.com>
2022-09-16 12:15:57 -0700

    path and selenium 4.4 updates

--- hour_filler.py
+++ hour_filler.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 
 import time
 import json
@@ -13,13 +14,14 @@ import warnings
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
 DRIVER_PATH = '/Users/brianbarry/Desktop/computing/chromedriver'
-AUTH_PATH = '/Users/brianbarry/Desktop/computing/tokens/ucsd.json'
+AUTH_PATH = '/Users/brianbarry/Desktop/computing/tokens/account_tokens.json'
 URL = 'https://urldefense.com/v3/__https://ecotimecampus.ucsd.edu__;!!LLK065n_VXAQ!xDdhgEjrgdTw8fuonI195LYjZIvs0GauFxk7Vqzxe-0c_IqGnmi7o8986XPruexnUw$'
 
 
 with open(AUTH_PATH, 'r') as f:
     login = json.load(f)
-    usr, pwd = login['USR'], login['PWD']
+    usr = 'BFBARRY@health.ucsd.edu'
+    pwd = login[usr]
     
     
 def work_days(start_date):
@@ -53,37 +55,37 @@ if __name__ == '__main__':
         start_date = two_weeks_ago
     else:
         start_date = input('Enter starting date e.g., 05-02-2022 : \n')
-    wd = webdriver.Chrome(DRIVER_PATH)
+    wd = webdriver.Chrome(executable_path=DRIVER_PATH)
     wd.get(URL)
 
     # login page
-    wd.find_element_by_id('ssousername').send_keys(usr)
-    wd.find_element_by_id('ssopassword').send_keys(pwd)
-    wd.find_element_by_name('_eventId_proceed').click()
+    wd.find_element(By.ID, 'ssousername').send_keys(usr)
+    wd.find_element(By.ID, 'ssopassword').send_keys(pwd)
+    wd.find_element(By.NAME, '_eventId_proceed').click()
 
     # auth page - DO THIS MANUALLY
     # time.sleep(6)
-    # wd.find_element_by_xpath('/html/body/div/div/div[1]/div/form/div[1]/fieldset/div[2]/button').click()
+    # wd.find_element(By.XPATH, /html/body/div/div/div[1]/div/form/div[1]/fieldset/div[2]/button').click()
     while not input('Done with auth?'):
         ...
         
     # ecotime
     ## nav bar
     switch_frames(wd, 'frmTop')
-    wd.find_element_by_link_text('Employee Tasks').click()
-    wd.find_element_by_partial_link_text('Timesheet').click()
+    wd.find_element(By.LINK_TEXT, 'Employee Tasks').click()
+    wd.find_element(By.PARTIAL_LINK_TEXT,  'Timesheet').click()
 
     ## timesheet
 
     for d in work_days(start_date):
         switch_frames(wd, 'frmBottom')
 
-        wd.find_element_by_partial_link_text(d).click()
-        Select(wd.find_element_by_name('cbInHrs1')).select_by_value('8')
-        Select(wd.find_element_by_name('cbOutHrs1')).select_by_value('5')
-        Select(wd.find_element_by_name('cbOutAP1')).select_by_value('P.M.')
-        Select(wd.find_element_by_name('MEALBREAK1')).select_by_value('60')
+        wd.find_element(By.PARTIAL_LINK_TEXT,  d).click()
+        Select(wd.find_element(By.NAME, 'cbInHrs1')).select_by_value('8')
+        Select(wd.find_element(By.NAME, 'cbOutHrs1')).select_by_value('5')
+        Select(wd.find_element(By.NAME, 'cbOutAP1')).select_by_value('P.M.')
+        Select(wd.find_element(By.NAME, 'MEALBREAK1')).select_by_value('60')
         
         # save
         switch_frames(wd, 'frmTop')
-        wd.find_element_by_xpath('/html/body/span/table/tbody/tr/td[1]/a').click()
+        wd.find_element(By.XPATH, '/html/body/span/table/tbody/tr/td[1]/a').click()
